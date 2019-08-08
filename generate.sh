41823df3c6a902f9d25173614ca1b3b312ec9338
Denis Feklushkin <denis.feklushkin@gmail.com>
2019-08-08 10:25:35 +0700

    Root package file generation added

--- generate.sh
+++ generate.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-set -ex
+set -e
 
 DSTEP=$1
 SRCDIR=$2
@@ -11,11 +11,17 @@ export LIBSODIUMD
 rm -rf ${LIBSODIUMD}/*
 
 # Generate D files from C headers
-find "$SRCDIR/src/libsodium/include" -maxdepth 2 -xtype f -name '*.h' \
+find "$SRCDIR/src/libsodium/include/sodium" -maxdepth 1 -xtype f -name '*.h' \
     -exec ${DSTEP} '{}' \; \
     -exec sh -c 'mv $(dirname "$0")/*.d ${LIBSODIUMD}' {}  \;
 
 mv ${LIBSODIUMD}/export.d ${LIBSODIUMD}/export_.d # as it conflicts with a D keyword
+
+# Turn ULL constants into UL:
+for file in $(find ${LIBSODIUMD} -xtype f -name "*.d")
+do
+sed -i -e 's/\([[:digit:]]\)ULL/\1UL/g' $file
+done
 
 # Adding module documentation, module name, and standard import
 for file in $(find ${LIBSODIUMD} -xtype f -name "*.d")
@@ -37,10 +43,6 @@ import libsodium.export_;
 " | cat - $file > $file.tmp && mv $file.tmp $file
 done
 
-# Turn ULL constants into UL:
-for file in $(find ${LIBSODIUMD} -xtype f -name "*.d")
-do
-sed -i -e 's/\([[:digit:]]\)ULL/\1UL/g' $file
-done
-
-# Generate sodium.d
+# Generate root package file
+echo "/// D language bindings for libsodium\n///\n/// License: ISC (see LICENSE.txt)\nmodule libsodium;\n" >> ${LIBSODIUMD}/package.d
+grep -re 'module ' --no-filename --include='*.d' ${LIBSODIUMD} | sed 's/module/public import/' | grep -v '#public import libsodium.sodium;' | sort >> ${LIBSODIUMD}/package.d
